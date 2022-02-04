2f5e3014cc27069be77c2a9bec10d47eba18f30d
kojibello <kojibello@macbook-pro.myfiosgateway.com>
2022-02-04 01:12:52 -0800

    cleaning up

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+enabled_metrics = [
+  "GroupDesiredCapacity", "GroupInServiceCapacity", "GroupPendingCapacity",
+  "GroupMinSize", "GroupMaxSize", "GroupInServiceInstances", "GroupPendingInstances",
+  "GroupStandbyInstances", "GroupStandbyCapacity", "GroupTerminatingCapacity",
+  "GroupTerminatingInstances", "GroupTotalCapacity", "GroupTotalInstances"
+]
--- variable.tf
+++ variable.tf
@@ -7,21 +7,20 @@ variable "aws_region" {
   description = "Region to which this resources would be created"
 }
 
-
 variable "asg_max_size" {
-  description = "Indicates whether capacity rebalance is enabled"
+  description = "maximum capacity for for jenkins agents"
   type        = number
   default     = 3
 }
 
 variable "asg_min_size" {
-  description = "Setting this causes Terraform to wait for this number of instances to show up healthy in the ELB only on creation. Updates will not wait on ELB instance number changes"
+  description = "minimum capacity for for jenkins agents"
   type        = number
   default     = 1
 }
 
 variable "asg_desired_capacity" {
-  description = "Setting this will cause Terraform to wait for exactly this number of healthy instances in all attached load balancers on both create and update operations. Takes precedence over `min_elb_capacity` behavior."
+  description = "desired capacity for for jenkins agents"
   type        = number
   default     = 2
 }
@@ -32,21 +31,12 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "user_data" {
-  description = "(LC) The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument nor when using Launch Templates; see `user_data_base64` instead"
-  type        = string
-  default     = null
+variable "ansible_version" {
+  description = "version of the playbook that would be run againt the agent"
+  default     = "3.7.10"
 }
 
-variable "public_key_path" {
-  type        = string
-  description = "local keypair"
-  default     = "/Users/kojibello/.ssh/id_rsa.pub"
-}
-
-variable "ami" {
-  type        = string
-  sensitive   = true
-  description = "ami id specific only to for jenkins/sonarqube build in us-east-1"
-  default     = "ami-0a8b4cd432b1c3063"
+variable "enabled_metrics" {
+  type        = list(any)
+  description = "Metrics that would be monitored by cloudwatch"
 }
